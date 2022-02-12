3757e256c93f9ce7a918ea67e76c0ba6cc47c9df
dsigner7 <martin.emails@protonmail.com>
2022-02-12 01:19:07 -0800

    the UI updates now

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -4,7 +4,7 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-
+populateUI();
 
 let ticketPrice = +movieSelect.value;
 
@@ -31,6 +31,23 @@ function updateSelectedCount(){
 	total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+function populateUI(){
+	const selectedSeats = JSON.parse(localStorage.getItem('asientosSelectos'));
+
+	if(selectedSeats !== 'null' && selectedSeats.length > 0){
+		seats.forEach((seat, index) => {
+			if(selectedSeats.indexOf(index) > -1){
+				seat.classList.add('selected');
+			}
+		})
+	}
+
+	const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+	if(selectedMovieIndex !== null){
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
+}
 
 // Movie select event
 movieSelect.addEventListener('change', e => {
@@ -47,3 +64,6 @@ container.addEventListener('click', e =>{
 		updateSelectedCount();
 	}
 })
+
+// Initial count and total set
+updateSelectedCount();
